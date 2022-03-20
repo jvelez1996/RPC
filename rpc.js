bfd8c6a631a4cba3cc831837a8199536871a44b8
jvelez1996 <miahvelez@yahoo.com>
2022-03-20 11:17:07 -0600

    Finally almost done

--- rpc.js
+++ rpc.js
@@ -1,6 +1,6 @@
-let playerContainer = 0
-let computerContainer = 0
-let roundsPlayed = 0
+let playerContainer = 0;
+let computerContainer = 0;
+let roundsPlayed = 0;
 
 function computerPlay () {
     let values = ['rock', 'paper', 'scissors'];
@@ -10,47 +10,53 @@ function computerPlay () {
 
 
 function playRound (playerSelection, computerSelection) {
+
      if (playerSelection === computerSelection) {
-        return 'It is a Tie!';
+       return 'It is a Tie!';
     }
 
-     if (playerSelection === 'rock' && computerSelection === 'paper') {
+     else if (playerSelection === 'rock' && computerSelection === 'paper') {
         computerContainer++; return 'You Lost!';
      }
    
-     if (playerSelection === 'rock' && computerSelection === 'scissors') {
-         playerContainer++; return 'You Won!';
+     else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerContainer++; return 'You Won!';
      }
 
-     if (playerSelection === 'scissors' && computerSelection === 'paper') {
-         playerContainer++; return "You Won!";
+     else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerContainer++; return "You Won!";
      }
 
-     if (playerSelection === 'scissors' && computerSelection === 'rock') {
-         computerContainer++; return 'You Lost!';
+     else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerContainer++; return 'You Lost!';
      }
 
-     if (playerSelection === 'paper' && computerSelection === 'rock') {
-         playerContainer++; return 'You Win!';
+     else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerContainer++; return 'You Win!';
      }
 
-     if (playerSelection === 'paper' && computerSelection === 'scissors') {
-         computerContainer++; return 'You Lost!';
+     else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerContainer++; return 'You Lost!';
      }
 
 }
 
-function game () {
-    for (let i = 0; i < 5; i++) {
+function game(){
+
+    while (roundsPlayed < 5){
+        let playerSelection = prompt("Rock, Paper, or Scissors?");
+        let playerLower = playerSelection.toLowerCase();
+        let computerSelection = computerPlay();
         playRound (playerSelection, computerSelection);
+        roundsPlayed++;
+        console.log (playRound (playerSelection, computerSelection));
+        console.log ("Rounds Played " + roundsPlayed)
+        console.log ("Player Score " + playerContainer);
+        console.log ("Computer Score " + computerContainer);
     }
 }
 
-let playerSelection = prompt('rock, paper, or scissors?');
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
 console.log(game ());
-//console.log("Your Score is " + playerContainer);
-//console.log("The Computer Score is " + computerContainer);
-//console.log("Number of Rounds Played " + roundsPlayed);
+
 
